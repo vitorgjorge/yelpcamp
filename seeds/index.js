aac5cd7633e3d3ba71ed9ca4cfcac686c071d2cc
vitorgjorge <vitorgjorge@gmail.com>
2023-07-04 22:53:14 -0300

    fixed index page img size

--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,7 @@ app.set("views", path.join(__dirname, "views"));
 const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+    await mongoose.connect("mongodb+srv://vitor:JvwBlRdi3E9jkWpt@cluster0.ptv7lk5.mongodb.net/?retryWrites=true&w=majority");
     console.log("DB CONNECTION OPEN");
 }
 const Campground = require("../models/campground");
@@ -24,7 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: "6491f0367475fe52e9058473",
+            author: "64a4bf80e6939020606ec2ec",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             images: [
